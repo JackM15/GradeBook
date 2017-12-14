f64af8a1b6cadd7b58d8d6daac3bccb1342d5195
Jack <jackm550@googlemail.com>
2017-12-14 15:24:15 +0000

    Methods & String Formatting explored.

--- Grades/Program.cs
+++ Grades/Program.cs
@@ -12,9 +12,19 @@ namespace Grades
             book.AddGrade(75);
 
             GradeStatistics stats = book.ComputeStatistics();
-            Console.WriteLine($"Average Grade: {stats.AverageGrade}");
-            Console.WriteLine($"Highest Grade: {stats.HighestGrade}");
-            Console.WriteLine($"Lowest Grade: {stats.LowestGrade}");
+            WriteResult("Average", stats.AverageGrade);
+            WriteResult("Highest", (int)stats.HighestGrade);
+            WriteResult("Lowest", stats.LowestGrade);
+        }
+
+        static void WriteResult(string description, float result)
+        {
+            Console.WriteLine($"{description}: {result:F2}");
+        }
+
+        static void WriteResult(string description, int result)
+        {
+            Console.WriteLine(description + ": " + result);
         }
     }
 }
